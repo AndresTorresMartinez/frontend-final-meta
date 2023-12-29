6f6a2e7ab58b2fa353f5ea7e7404a3a5e0f24618
Andres Torres <atorres@variadores.com.co>
2023-12-29 09:03:18 -0500

    Añádidos componentes principales

---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+const Nav = () => {
+  return (
+    <>
+    <img ></img>
+      <ul>
+        <li>
+          <a href="/home">Homepage</a>
+        </li>
+        <li>
+          <a href="/about">About Us</a>
+        </li>
+        <li>
+          <a href="/blog">Reserve</a>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Nav;
