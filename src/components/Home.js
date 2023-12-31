b531b261f2bac80778aa050c78fabe090c1069f0
Andres Torres <ctorres437@hotmail.com>
2023-12-31 12:23:45 -0500

    Home / Reservation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,52 @@
+// Home.js
+
+import React from 'react';
+
 const Home = () => {
-    return <>
-    HOME
-    </>;
-  };
-  
-  export default Home;
+  return (
+    <div>
+      {/* Sección del Plato del Día */}
+      <section>
+        <h2>Special Dish of the Day</h2>
+        <p>Today's featured dish: Grilled Salmon with Lemon Herb Sauce</p>
+        <p>Available for a limited time. Don't miss out!</p>
+      </section>
+
+      {/* Sección de Platos de Muestra */}
+      <section>
+        <h2>Featured Dishes</h2>
+        <div className="dish-cards">
+          <div className="dish-card">
+            <img src="paella.png" alt="Paella" />
+            <h3>Paella</h3>
+            <p>Traditional Spanish rice dish with a mix of seafood and meat.</p>
+          </div>
+          <div className="dish-card">
+            <img src="ratatouille.png" alt="Ratatouille" />
+            <h3>Ratatouille</h3>
+            <p>A French Provençal stewed vegetable dish, originating in Nice.</p>
+          </div>
+          {/* Agrega más tarjetas según sea necesario */}
+        </div>
+      </section>
+
+      {/* Sección de Testimonios de Usuarios */}
+      <section>
+        <h2>User Testimonials</h2>
+        <div className="testimonial-cards">
+          <div className="testimonial-card">
+            <p>"The food was amazing! I'll definitely be back."</p>
+            <p>- Jane Doe</p>
+          </div>
+          <div className="testimonial-card">
+            <p>"Great service and delicious dishes. Highly recommend!"</p>
+            <p>- John Smith</p>
+          </div>
+          {/* Agrega más tarjetas de testimonios según sea necesario */}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
